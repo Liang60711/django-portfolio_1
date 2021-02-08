d4c8ca5d3b689b9b392ffc7f7b73fb55ca7de1b5
Liang <liang60711@gmail.com>
2021-02-08 21:12:06 +0800

    deploy

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '#r&tk#t4_=b_wa9%ez!cr0mseq(q2%mkk!fi2e#%6@g685-d=3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Deploy on HEROKU
 ALLOWED_HOSTS = [
@@ -150,7 +150,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
