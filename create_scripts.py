8a4d6f63ad2c4f859cbb84583f0d22447f6fcd55
Liang <liang60711@gmail.com>
2021-02-08 23:54:25 +0800

    deploy

--- create_scripts.py
+++ create_scripts.py
@@ -35,16 +35,16 @@ Item.objects.all().delete()
 
 # f = File(open(os.path.join(settings.BASE_DIR, 'media', '5.jpg'), 'rb'))
 
-f_heroku = File(open(settings.MEDIA_ROOT + '\\5.jpg', 'rb'))
+f_heroku = File(open(os.path.join(settings.BASE_DIR, 'media', '5.jpg'), 'rb'))
 print(f_heroku)
 
-for i in range(12):
-    Item.objects.create(
-        title='Item '+str(i),
-        price=random.randint(1,9)*100,
-        category=random.choices(CATEGORY_CHOICES)[0][0],
-        label=random.choices(LABEL_CHOICES)[0][0],
-        description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pretium ante erat, vitae sodales mi varius quis. Etiam vestibulum lorem vel urna tempor, eu fermentum odio aliquam. Aliquam consequat urna vitae ipsum pulvinar, in blandit purus eleifend.',
-        slug=str(i),
-        image=f_heroku,
-    )
+# for i in range(12):
+#     Item.objects.create(
+#         title='Item '+str(i),
+#         price=random.randint(1,9)*100,
+#         category=random.choices(CATEGORY_CHOICES)[0][0],
+#         label=random.choices(LABEL_CHOICES)[0][0],
+#         description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pretium ante erat, vitae sodales mi varius quis. Etiam vestibulum lorem vel urna tempor, eu fermentum odio aliquam. Aliquam consequat urna vitae ipsum pulvinar, in blandit purus eleifend.',
+#         slug=str(i),
+#         image=f_heroku,
+#     )
